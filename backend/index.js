c2301d66a1b30603e23a57ed95d06c7a29e4e20c
Alexis Chesneau <alexischesneau64@gmail.com>
2024-04-15 00:48:15 +0200

    ajout middlewaresDebug

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const middlewaresDebug = require("./middlewares/middlewares");
+const middlewaresDebug = require("./middlewares/middlewaresDebug");
 const express = require("express");
 const cors = require("cors");
 const app = express();
